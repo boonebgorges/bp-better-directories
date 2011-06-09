0b86647f818a5887b8adb661d6bf795318af62f9
Boone Gorges <boonebgorges@gmail.com>
2011-06-09 11:38:19 -0400

    Collects filters into a single method. Adds UI method skeleton

--- bpbd.php
+++ bpbd.php
@@ -37,8 +37,11 @@ class BPBD {
 		
 		if ( $is_members && !bp_is_single_item() ) {			
 			// Add the sql filters
-			add_filter( 'bp_core_get_paged_users_sql', array( $this, 'paged_users_sql_filter' ), 10, 2 );
-			add_filter( 'bp_core_get_total_users_sql', array( $this, 'total_users_sql_filter' ), 10, 2 );
+			add_filter( 'bp_core_get_paged_users_sql', array( $this, 'users_sql_filter' ), 10, 2 );
+			add_filter( 'bp_core_get_total_users_sql', array( $this, 'users_sql_filter' ), 10, 2 );
+			
+			// Add the filter UI
+			add_action( 'bp_before_members_loop', array( $this, 'filter_ui' ) );
 		}
 	}
 	
@@ -65,39 +68,51 @@ class BPBD {
 		// Todo: sort order?
 	}
 	
-	function paged_users_sql_filter( $s, $sql ) {
+	function users_sql_filter( $s, $sql ) {
 		global $bp, $wpdb;
 		
+		$bpbd_select = array();
 		$bpbd_from = array();
 		$bpbd_where = array();
 		$counter = 1;
 		
+		// Build the additional queries
 		foreach( $this->get_params as $field_id => $field ) {
 			$table_shortname = 'bpbd' . $counter;
+						
+			// Since we're already doing the join, let's bring the extra content into
+			// the template. This'll be unset in the total_users filter
+			$bpbd_select[] = $wpdb->prepare( ", {$table_shortname}.value as {$field['slug']}" );
 			
 			$bpbd_from[] = $wpdb->prepare( "INNER JOIN {$bp->profile->table_name_data} {$table_shortname} ON ({$table_shortname}.user_id = u.ID)" );
 			
-			// todo: like vs in vs equals!
+			// todo: LIKE vs IN vs = (maybe with NOTs as well?)
 			$bpbd_where[] = $wpdb->prepare( "AND {$table_shortname}.field_id = %s AND {$table_shortname}.value = %s", $field['id'], $field['value'] );
 			
 			$counter++;
 		}
-		
+
 		if ( !empty( $bpbd_from ) && !empty( $bpbd_where ) ) {
+			// The total_sql query won't have this index
+			if ( isset( $sql['select_main'] ) )
+				$sql['select_main'] .= ' ' . implode( ' ', $bpbd_select );			
+			
 			$sql['from'] .= ' ' . implode( ' ', $bpbd_from );
 			$sql['where'] .= ' ' . implode( ' ', $bpbd_where );
-			
+					
 			$s = join( ' ', (array)$sql );
 		}
 		
 		return $s;
+	}	
+	
+	function filter_ui() {
+	?>
+		<div id="bpbd-filters" style="color:#000;height: 100px">
+		
+		</div>
+	<?php
 	}
-	
-	function total_users_sql_filter( $s, $sql ) {
-		return $s;
-	}
-	
-	
 }
 
 ?>
